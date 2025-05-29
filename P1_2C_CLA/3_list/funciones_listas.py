14f23dc7fb7553a63b5c3b429dfda874c2c2b5c6
DagFiscal <48801403+dagfiscal@users.noreply.github.com>
2025-05-29 10:30:58 -0600

    1er commit 29_05_25
    En este se guardaron los archivos de la primera unidad lo de funciones, módulos, paquetes y listas

---
+++ P1_2C_CLA/3_list/funciones_listas.py
@@ -0,0 +1,17 @@
+"""  
+ List (Array)
+ son colleciones o conjunto de datos/valores bajo un mismo nombre, para acceder a los valores se hace con un indice numerico 
+
+ Nota: sus valores si son modificables
+
+ La lista es una colección ordenada y modificable. Permite miembros duplicados.
+
+"""
+
+import os 
+os.system("clear")
+
+#Funciones más comunes en las listas
+
+
+
