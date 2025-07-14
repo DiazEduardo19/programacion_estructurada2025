3cb4d7dcaee480d5adc7cb1c79f0444378c17027
DiazEduardo19 <eduardo_3041240475@utd.edu.mx>
2025-07-13 23:21:24 -0600

    Delete P3_programacion/1_proyecto_peliculas_v3 directory

--- P3_programacion/1_proyecto_peliculas_v3/Codigos_extra.py
+++
@@ -1,6 +0,0 @@
-peliculas=[]
-respuesta="si"
-while respuesta=="si":
-    peliculas.append(input("Bienvenido a al catalogo de peliculas añade cuantas quieras:"))
-    respuesta=input("Deseas agregar otra pelicula:").lower()
-print(peliculas)
